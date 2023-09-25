f665d44d8cb40d0a7ad51911f0aa2aa232047f8c
AlexisPalavecino <98364131+AlexisPalavecino@users.noreply.github.com>
2023-09-25 11:50:13 -0300

    Update StackNavigator.js

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -12,12 +12,12 @@ function StackNavigator() {
             <NavigationContainer>
                 <Stack.Navigator initialRouteName="Home" screenOptions={({route, navigation}) => ({
                     headerShown: false,
-                    // header:() => (
-                    //     <View style={{flexDirection:'row', marginTop:50}}>
-                    //         <Button onPress={() => navigation.goBack()} title= "Go Back" />
-                    //         <Header title={route.name}/>
-                    //     </View>
-                    // )
+                    header:() => (
+                         <View style={{flexDirection:'row', marginTop:50}}>
+                             <Button onPress={() => navigation.goBack()} title= "Go Back" />
+                            <Header title={route.name}/>
+                         </View>
+                     )
                 })}>
                     <Stack.Screen name='Home' component={Home} />
                     <Stack.Screen name='Products' component={Products} />
